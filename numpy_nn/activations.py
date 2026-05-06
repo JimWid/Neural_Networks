a198d41754c3fbb3af6a39165f0b94b9bd707f11
Widmark La Rosa <jimmylr80@gmail.com>
2026-05-05 21:24:09 -0400

    adding BatchNormalization

--- numpy_nn/activations.py
+++ numpy_nn/activations.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy_nn.layers import Layer
+from numpy_nn.layers import Layer, Dense
 
 # Tanh Function
 def tanh(x):
@@ -56,3 +56,53 @@ class ReLU(Layer): # Rectified Linear Unit (ReLU)
 
     def backward(self, output_gradient, learning_rate):
         return output_gradient * (self.input > 0)
+
+class BatchNormalization(Layer): # Batch Normalization!
+    def __init__(self, epsilon=1e-8):
+        super().__init__()
+
+        self.epsilon = epsilon
+        self.scales = None
+        self.bias = None
+
+    def forward(self, input):
+        self.input = input
+        self.out_shape = input.shape
+
+        self.mean = self.input.mean(axis=0)
+        self.variance = 1. / np.sqrt(self.input.var(axis=0) + self.epsilon)
+
+        self.input_norm = (self.input - self.mean) * self.variance
+        self.output = self.input_norm.copy()
+
+        if self.scales is not None:
+            self.output *= self.scales
+
+        if self.bias is not None:
+            self.output +=self.bias
+            
+        return self.output
+     
+    def backward(self, output_gradient, learning_rate):
+        input_shape = self.input.shape[0]
+        inv_shape = 1. / input_shape
+
+        bias_gradient = output_gradient.sum(axis=0)
+        scales_gradient = (output_gradient * self.input_norm).sum(axis=0)
+
+        if self.scales is not None:
+            output_gradient *= self.scales
+
+        mean_delta = (output_gradient * (-self.variance)).sum(axis=0)
+        variance_delta = (output_gradient * (self.input - self.mean)).sum(axis=0) * -0.5 * self.variance**3
+
+        delta = (output_gradient * self.variance
+                + variance_delta * 2 * (self.input - self.mean) * inv_shape
+                + mean_delta * inv_shape)
+
+        if self.scales is not None:
+            self.scales -= learning_rate * scales_gradient
+        if self.bias is not None:
+            self.bias -= learning_rate * bias_gradient
+
+        return delta
